3c634e1414bac3a9847f3cfd8887dff20b2c4a6a
pmea <kurugan04@hotmail.fr>
2015-12-18 12:44:34 +0100

    creation du test de base, le meme que celui de wikipedia en

--- src/test_needleman.m
+++ src/test_needleman.m
@@ -1,2 +1,15 @@
 % script de test de l'algo de Needlenam
 
+
+chaineA= 'GCATGCU';
+chaineB= 'GATTACA';
+
+m_sim_simple= [1 -1 -1 -1; -1 1 -1 -1; -1 -1 1 -1; -1 -1 -1 1];
+m_sim_complex= [10 -1 -3 -4; -1 7 -5 -3; -3 -5 9 0; -4 -3 0 8 ];
+
+gap= -1;
+
+[m_res, score]= f_needlenam(chaineA, chaineB, m_sim, gap);
+
+disp(score);
+imagesc(m_res);
