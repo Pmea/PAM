8ea3477e037a11403aa8cd08a952f213afea1c23
pmea <kurugan04@hotmail.fr>
2015-12-18 12:52:49 +0100

    initalisation de la matrice needleman

--- src/f_needlenam.m
+++ src/f_needlenam.m
@@ -3,9 +3,22 @@ function [m_res, score]= f_needlenam(chaineA, chaineB, m_sim, gap)
 % matrice de similarite (matrice de poids)
 % poid du gap
 
+score=0;
+
 %initialisation
 
+len_A= length(chaineA);
+len_B= length(chaineB);
 
+m_res= zeros(len_A+1, len_B+1); % avec length+1 car il y a la case vite
+                                % au debut du mot
+for k=0:len_A
+    m_res(k+1,1)= gap * k;
+end
+
+for k=0:len_B
+    m_res(1,k+1)= gap * k;
+end
 
 % calcule de la matrice
 
--- src/test_needleman.m
+++ src/test_needleman.m
@@ -9,7 +9,7 @@ m_sim_complex= [10 -1 -3 -4; -1 7 -5 -3; -3 -5 9 0; -4 -3 0 8 ];
 
 gap= -1;
 
-[m_res, score]= f_needlenam(chaineA, chaineB, m_sim, gap);
+[m_res, score]= f_needlenam(chaineA, chaineB, m_sim_simple, gap);
 
 disp(score);
 imagesc(m_res);
