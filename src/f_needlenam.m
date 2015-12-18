512460a065a957d4dd2b303148a96f9fd0810a11
pmea <kurugan04@hotmail.fr>
2015-12-18 13:47:53 +0100

    calcule de la matrice fonctionne / besoin de reorpganiser le code avec sous-fonction

--- src/f_needlenam.m
+++ src/f_needlenam.m
@@ -1,6 +1,8 @@
-function [m_res, score]= f_needlenam(chaineA, chaineB, m_sim, gap)
+function [m_res, score]= f_needlenam(chaineA, chaineB, m_sim, m_cor, gap)
 % chaineA et chaineB, les chaines comparer
 % matrice de similarite (matrice de poids)
+% matrice de correspondence entre l'alphabet et les positions dans la
+% matrice de similarite
 % poid du gap
 
 score=0;
@@ -22,6 +24,35 @@ end
 
 % calcule de la matrice
 
+for k=2:len_B+1
+    disp(k);
+    for l=2:len_A+1
+        val_haut= m_res(k-1, l) + gap;
+        val_gauche= m_res(k, l-1) + gap;
+        
+        % recherche ind_A
+        for m=1: size(m_cor,1)         % faire de la recheche une fonction
+            if m_cor(m,1) == chaineA(l-1)
+                ind_A= m;
+            end
+        end
+        
+        % recherche ind_B
+        for m=1: size(m_cor,1)         % faire de la recheche une fonction
+            if m_cor(m,1) == chaineB(k-1)
+                ind_B= m;
+            end
+        end
+                
+        val_diago= m_res(k-1,l-1) + m_sim(ind_A, ind_B);
+        
+        disp([val_gauche val_haut val_diago]);
+
+        m_res(k,l)= max([val_gauche val_haut val_diago]);
+    end
+     imagesc(m_res);
+     %pause;
+end
 
 % calcule du resultat
 
--- src/test_needleman.m
+++ src/test_needleman.m
@@ -1,15 +1,19 @@
 % script de test de l'algo de Needlenam
-
+close all;
+clear all;
 
 chaineA= 'GCATGCU';
 chaineB= 'GATTACA';
 
-m_sim_simple= [1 -1 -1 -1; -1 1 -1 -1; -1 -1 1 -1; -1 -1 -1 1];
-m_sim_complex= [10 -1 -3 -4; -1 7 -5 -3; -3 -5 9 0; -4 -3 0 8 ];
+% definition de l'alphabet
+% l'ordre: A G C T
+m_cor= ['A1'; 'G2'; 'C3'; 'T4'; 'U5'];
+m_sim_simple= [1 -1 -1 -1 -1; -1 1 -1 -1 -1; -1 -1 1 -1 -1; -1 -1 -1 1 -1; -1 -1 -1 -1 1];
+%m_sim_complex= [10 -1 -3 -4; -1 7 -5 -3; -3 -5 9 0; -4 -3 0 8 ];
 
 gap= -1;
 
-[m_res, score]= f_needlenam(chaineA, chaineB, m_sim_simple, gap);
+[m_res, score]= f_needlenam(chaineA, chaineB, m_sim_simple, m_cor, gap);
 
 disp(score);
 imagesc(m_res);
