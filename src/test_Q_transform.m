6921d353fe9e24dca3be0a51cab21d108605ba85
pmea <kurugan04@hotmail.fr>
2016-01-11 12:49:32 +0100

    lisibilité test_q_transform

--- src/test_Q_transform.m
+++ src/test_Q_transform.m
@@ -1,8 +1,7 @@
 [sig, Fe]= audioread('song.wav');
-%Fe= 44100;
-Q=1;
 Nfft= 2^12;
 freq_la_ref= 440;
-%sig= zeros(1, Fe*10);
 
-sepctrum= f_Q_transform(sig, Fe, Q, Nfft, freq_la_ref);
+Q=1;
+
+sepctrum= f_Q_transform(sig, Fe, Q, freq_la_ref);
