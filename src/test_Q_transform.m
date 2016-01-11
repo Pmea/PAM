24da7eae58536743d54b65c3d460981161b902c6
pmea <kurugan04@hotmail.fr>
2016-01-11 12:28:46 +0100

    ajout lecture a partir d'un fichier wav pour le test

--- src/test_Q_transform.m
+++ src/test_Q_transform.m
@@ -1,8 +1,8 @@
-
-Fe= 44100;
+[sig, Fe]= audioread('song.wav');
+%Fe= 44100;
 Q=1;
 Nfft= 2^12;
 freq_la_ref= 440;
-sig= zeros(1, Fe*10);
+%sig= zeros(1, Fe*10);
 
 sepctrum= f_Q_transform(sig, Fe, Q, Nfft, freq_la_ref);
