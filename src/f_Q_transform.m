a46d2bace2ad69975b764c665b3301028e506c6b
pmea <kurugan04@hotmail.fr>
2016-01-12 12:17:32 +0100

    amelioration des performences

--- src/f_Q_transform.m
+++ src/f_Q_transform.m
@@ -3,17 +3,15 @@ function [m_spect]= f_Q_transform(v_sig, Fe, Q, freq_la_ref)
 % Varible
 v_len_sig= length(v_sig);
 
-Cw=1.82;
+Cw=1.82; % le Cw de la fenetre de Hann
 % Calcule des frequences par rapport au fonction midi
-note_min= 21;   % valeur midi de note min et max
-note_max= 127;
+note_min= 45;   % valeur midi de note min et max
+note_max= 104;
 nb_note= note_max - note_min +1;
 
-v_mk= (note_min : note_max);  %les note midi du A0 au G#8
+v_mk= (note_min : note_max);  %les notes midi 
 v_f= freq_la_ref * 2.^((v_mk- 69)/12); % les frequences des differentes bandes
-
-% figure;
-% plot(v_f);
+v_f_red= v_f/Fe;
 
 % Creation des longeurs des fenetres
 v_len_fen= floor((Q * Cw) ./ (v_f/Fe));  % la longeur de la fenetre pour chaque bande
@@ -29,23 +27,18 @@ for k= 1: nb_note
     m_fen(k, deb : fin)= v_fen_tmp(1:fin-deb+1);
 end
 
-% figure;
-% hold on;
-% plot(m_fen(1,:));
-% plot(m_fen(13,:));
-% title('Les fenetres');
-% hold off;
 
-% Application de la fft
+% Application de la CQT
 hop = floor(min_len_fen / 3);  
 
 frames= round((v_len_sig - max_len_fen)/hop-1);  %nb de frame pour la tfct
 m_spect= zeros(frames, nb_note);
 
-disp(frames);
+disp(frames);         % affichage
+disp('appuyez pour lancer');
 pause();
 
-v_sig= v_sig';      %on transpose le signal un fois pour ne pas avoir a le faire a chaque fois
+v_sig= v_sig'; % transposition du vecteur colone en vercteur ligne
 for k= 1: frames
    deb= k*hop;
    fin= k*hop + max_len_fen - 1;
@@ -53,12 +46,11 @@ for k= 1: frames
 
    % compute de la CQT 
    for l=1:nb_note
-       m_spect(k,l)=  sum( m_fen(l,:) .* v_sig(deb:fin) .* exp(-1j * 2 * pi * v_f(l)/Fe * (deb:fin)));
+       v_e= exp(-1j * 2 * pi * v_f_red(l) * (deb:fin));
+       m_spect(k,l)=  sum( m_fen(l,:) .* v_sig(deb:fin) .* v_e);
    end
    
-   
 end
-figure;
-imagesc(abs(m_spect'));
 
+m_spect= m_spect';
 end
--- src/test_Q_transform.m
+++ src/test_Q_transform.m
@@ -2,10 +2,14 @@ close all;
 clear all;
 %[sig, Fe]= audioread('song.wav');
 Fe= 44100;
-sig= sin((1:2*Fe/3)/Fe*440*2*pi)';  %un sinus pur de 1/4 de seconde
+sig= sin((1:Fe)/Fe*440*2*pi)';
+
 plot(sig);
 freq_la_ref= 440;
 
 Q=10;
 
-sepctrum= f_Q_transform(sig, Fe, Q, freq_la_ref);
+spectrum= f_Q_transform(sig, Fe, Q, freq_la_ref);
+
+figure;
+imagesc(abs(flipud(spectrum)));
