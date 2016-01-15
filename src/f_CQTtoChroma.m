a11853c24d744c0f99a5cb5c0b9c315ea594a0c3
pmea <kurugan04@hotmail.fr>
2016-01-15 11:25:44 +0100

    correction nommage variable

--- src/f_CQTtoChroma.m
+++ src/f_CQTtoChroma.m
@@ -1,12 +1,12 @@
-function [m_chroma]= f_CQTtoChroma(spectrum, note_min )
+function [m_chroma]= f_CQTtoChroma(m_spectrum, note_min )
 
-   [acti_note, frames]= size(spectrum);
+   [acti_note, frames]= size(m_spectrum);
    m_chroma= zeros(frames, 12);
 
    for k= 1:frames
         for l=1:acti_note
             pos= mod(l+ note_min -1 , 12)+1;
-            m_chroma(k, pos)= chroma(k, pos) + spectrum(l, k);
+            m_chroma(k, pos)= m_chroma(k, pos) + m_spectrum(l, k);
         end
     end
 
