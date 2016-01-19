a2e73da32cb83326f1f3a50ccd9757edff3b17e9
pmea <kurugan04@hotmail.fr>
2016-01-19 17:15:11 +0100

    calcule de ma matrice fonctionnelle

--- src/f_smith_waterman_mine.m
+++ src/f_smith_waterman_mine.m
@@ -26,9 +26,55 @@ end
 
 % calcule de la matrice
 
+for k=2:len_B+1
+    for l=2:len_A+1
+        disp([k l]);
+        ind_A=recheche_cor(chaineA(l-1), m_cor);
+        ind_B=recheche_cor(chaineB(k-1), m_cor);
+        
+        % pour la diagonal (match ou dismatch)
+        val_diago= m_res(k-1,l-1) + m_sim(ind_A, ind_B);
+        % sauvegarde du prec ?
+        
+        % pour les colones
+        max_col= m_res(k-1, l) + gap * 1;
+        for col=1:k-1
+            if (m_res(k-col, l) + (gap * col)) > max_col
+                disp('colone !');
+                max_col= (m_res(k-col, l) + (gap * col)); 
+                % sauvegarde du prec ? 
+            end
+        end
+        
+        % pour les lignes
+        max_ligne= m_res(k,l-1) + gap * 1;        %on init a la prec du haut
+        for ligne=1:l-1
+            if (m_res(k, l-ligne) + (gap * ligne)) > max_ligne
+                disp('ligne !');
+                max_ligne= (m_res(k, l-ligne) + (gap * ligne));
+                % sauvegarde du prec ?
+            end
+        end 
+        
+        % selection du max
+        [m_res(k,l),~]= max([max_ligne val_diago max_col 0]);
+        
+        % savegarde du prec
+    end 
+end
 
 
 % calcule du resultat
+score= 1;
 
+end
 
+function indice = recheche_cor (car, m_cor)
+%retourne l'indice du caractere dans la matrice de similarite
+%a l'aide de la matrice de correspondance
+    for k=1: size(m_cor,1)
+        if m_cor(k,1) == car
+            indice = k;
+        end
+    end
 end
--- src/test_Q_transform2.m
+++ src/test_Q_transform2.m
@@ -1,21 +1,18 @@
 close all;
 clear all;
-[sig_tmp, Fe]= audioread('audio_gammepno.wav');
-sig= sig_tmp;%(1:4*Fe);
-%  Fe= 44100;
-%  freq=440;
-%  sig= (sin((1:Fe)/Fe*freq*2*pi) + sin((1:Fe)/Fe*freq*1.26*2*pi) + sin((1:Fe)/Fe*freq*1.5*2*pi) )' ;
+%[sig_tmp, Fe]= audioread('audio_gammepno.wav');
+%sig= sig_tmp;%(1:4*Fe);
+ Fe= 44100;
+ freq=440;
+ sig= (sin((1:Fe)/Fe*freq*2*pi) + sin((1:Fe)/Fe*freq*1.26*2*pi) + sin((1:Fe)/Fe*freq*1.5*2*pi) )' ;
 
 plot(sig);
 freq_la_ref= 440;
 
 Q=20;
-% 
-% note_midi_min= 33; 
-% note_midi_max= 116;
 
-note_midi_min=45;
-note_midi_max= 104;
+note_midi_min= 33;  % 45;
+note_midi_max= 116; % 104;
 
 chroma= f_Q_transform2(sig, Fe, Q, note_midi_min, note_midi_max, freq_la_ref);
 
