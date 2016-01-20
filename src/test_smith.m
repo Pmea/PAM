95e47402b89f146fbb9e50c28b16d8f107243766
pmea <kurugan04@hotmail.fr>
2016-01-20 11:44:46 +0100

    nettroyage et creation nouveau fichier

--- src/test_smith.m
+++
@@ -1,5 +0,0 @@
-%acc_cost=f_smith_waterman('salut', 'salsut', -3,-4);
-%acc_cost=f_smith_waterman('FATCATY', 'CATFAST', -3,-4); %
-
-acc_cost =f_smith_waterman( 'AGCACACA','ACACACTA', -1,-4);  %english wiki
-imagesc(acc_cost);
