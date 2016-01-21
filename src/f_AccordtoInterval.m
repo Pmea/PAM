245921c6af0b8c92fcf1b63a0a32ef717592ddba
pmea <kurugan04@hotmail.fr>
2016-01-21 11:55:57 +0100

    fonction accodtointerval fonctionnelle

--- src/f_AccordtoInterval.m
+++ src/f_AccordtoInterval.m
@@ -1,30 +1,52 @@
-function [v_inter] =f_AccordInterval (v_accord)
+function [v_inter] =f_AccordtoInterval (v_accord)
 
 % par convention on va dire qu'il y a toujours deux caracteres
 m_corres=[
-    'C 1'
-    'C#2'% meme note
-    'Db2'%
-    'D 3'
-    'D#4'%
-    'Eb4'%
-    'E 5'
-    'F 5'
-    'F#6'%
-    'Gb6'%
-    'G 7'
-    'G#8'%
-    'Ab8'%
-    'A 9'
-    'A#10'%
-    'Bb10'%
-    'B 11'
+    'C  1';
+    'C# 2';% meme note
+    'Db 2';%
+    'D  3';
+    'D# 4';%
+    'Eb 4';%
+    'E  5';
+    'F  6';
+    'F# 7';%
+    'Gb 7';%
+    'G  8';
+    'G# 9';%
+    'Ab 9';%
+    'A 10';
+    'A#11';%
+    'Bb11';%
+    'B 12';
 ];
 
-'a13'; 'b 
-v_inter= zeros(length(v_accord));
+v_inter= zeros(size(v_accord,1)-1,1);
+v_val_accord= zeros(size(v_accord,1), 1);
 
-
+for k=1:length(v_val_accord) %pour toute la chaine
+    s= v_accord(k,1:2);
+    
+    found=false;
+    for l=1:length(m_corres)  % on chercher le bon element
+        if strcmp(m_corres(l, 1:2), s(1:2))
+            v_val_accord(k)= str2double(m_corres(l, end-1:end));
+            found=true;
+            
+            if k > 1
+                v_inter(k-1)=  mod(v_val_accord(k)-v_val_accord(k-1) +6, 12);
+                v_inter(k-1)= v_inter(k-1) -6;
+            end
+            continue;
+        end
+    end
+    
+    if found == false
+        disp('Accord non trouvé');
+        disp('Erreur format suitre accord');
+        assert(true);
+    end
+end
 
 
 
--- src/test_AccordtoInterval.m
+++ src/test_AccordtoInterval.m
@@ -1,5 +1,16 @@
-% script de test de l'algo de smith_waterman
+% script de test pour la conversion la grille d'accords vers la liste 
+%des intervalles
+
+% format
+% A [b/#] [m/Ø] 
+% exemple [Ab ], [A  ], [A#m]
+
 close all;
 clear all;
 
+accords= ['C  ';'B  ';'B  '; 'C  '];
 
+inteval= f_AccordtoInterval(accords);
+
+figure;
+disp(inteval);
