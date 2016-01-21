b36772f0b177da9d43c90515bdb5cd50579d96e5
pmea <kurugan04@hotmail.fr>
2016-01-21 11:02:00 +0100

    ajout calcule des intervalles

---
+++ src/f_AccordtoInterval.m
@@ -0,0 +1,31 @@
+function [v_inter] =f_AccordInterval (v_accord)
+
+% par convention on va dire qu'il y a toujours deux caracteres
+m_corres=[
+    'C 1'
+    'C#2'% meme note
+    'Db2'%
+    'D 3'
+    'D#4'%
+    'Eb4'%
+    'E 5'
+    'F 5'
+    'F#6'%
+    'Gb6'%
+    'G 7'
+    'G#8'%
+    'Ab8'%
+    'A 9'
+    'A#10'%
+    'Bb10'%
+    'B 11'
+];
+
+'a13'; 'b 
+v_inter= zeros(length(v_accord));
+
+
+
+
+
+end
---
+++ src/test_AccordtoInterval.m
@@ -0,0 +1,5 @@
+% script de test de l'algo de smith_waterman
+close all;
+clear all;
+
+
