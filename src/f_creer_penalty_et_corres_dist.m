50cc1709944bceab4549a57f6fb8dd25f0cd9fa9
pmea <kurugan04@hotmail.fr>
2016-01-21 16:22:33 +0100

    correction fontion de penalty

--- src/f_creer_penalty_et_corres_dist.m
+++ src/f_creer_penalty_et_corres_dist.m
@@ -1,10 +1,10 @@
-function [m_seuil, m_penalty, m_corres]= f_creer_penalty_et_corres_dist(ext_gap, open_gap, c_choma_ref)
+function [m_seuil, m_penalty, m_corres]= f_creer_penalty_et_corres_dist(ext_gap, open_gap, c_chroma_ref)
 %les chromas doivent etre dans cet ordre
 
 m_corres=[
     'C   1';
     'C#  2';% meme note
-    'Db  2';%
+%    'Db  2';%
     'D   3';
 %     'D#  4';%
 %     'Eb  4';%
@@ -39,28 +39,31 @@ m_corres=[
 %     'Bm 24';
     ];
 
-
-% on fait la distance de chaque pour voir quoi choisir
-% par rapport a la moyenne 
-
+% on calcul la distante entre chaque chroma
 m_penalty= zeros(size(m_corres,1), size(m_corres,1));
+
+
+for c=1:size(c_chroma_ref,2);            %on normalise les chromas
+    sum_chroma= sum(c_chroma_ref{c});           
+    c_chroma_ref{c}=  c_chroma_ref{c}./ sum_chroma;
+end
 
 
 for k=1:size(m_corres,1)     % amelioration possible en parcourant les chromas
     for l=1:size(m_corres,1) % et non les cases (des calculs sont identique
-        m_penalty(k,l)= distance(c_choma_ref{k}, c_choma_ref{l});
+        m_penalty(k,l)= distance(c_chroma_ref{k}, c_chroma_ref{l});
     end
 end
 
 moyenne=mean(m_penalty(:));
 m_penalty= m_penalty - moyenne;
+m_penalty=  m_penalty;
 
 m_seuil=0;
 end
 
-function [score] = distance(v_choma1, v_choma2)
-    v_d= zeros(length(v_choma1, 1));  % les 12 notes
-    v_d= abs(v_choma1 - v_choma2);
+function [score] = distance(v_chroma1, v_chroma2)
+    v_d= abs(v_chroma1 - v_chroma2);
     
     score= sum(v_d);
 end
