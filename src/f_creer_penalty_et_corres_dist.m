ac4fdbc603b1059d3f4001fc018f58eca8911798
pmea <kurugan04@hotmail.fr>
2016-01-22 09:58:44 +0100

    suppresion fichier generé

--- src/f_creer_penalty_et_corres_dist.m
+++ src/f_creer_penalty_et_corres_dist.m
@@ -1,42 +1,42 @@
 function [m_seuil, m_penalty, m_corres]= f_creer_penalty_et_corres_dist(ext_gap, open_gap, c_chroma_ref)
 %les chromas doivent etre dans cet ordre
 
-m_corres=[
+m_corres=[          %pour le test ne garder que C, C#, D
     'C   1';
     'C#  2';% meme note
-%    'Db  2';%
+    'Db  2';%
     'D   3';
-%     'D#  4';%
-%     'Eb  4';%
-%     'E   5';
-%     'F   6';
-%     'F#  7';%
-%     'Gb  7';%
-%     'G   8';
-%     'G#  9';%
-%     'Ab  9';%
-%     'A  10';
-%     'A# 11';%
-%     'Bb 11';%
-%     'B  12';
-%     
-%     'Cm 13';        % accordm mineur
-%     'C#m14';%
-%     'Dbm14';%
-%     'Dm 15';
-%     'D#m16';%
-%     'Ebm16';%
-%     'Em 17';
-%     'Fm 18';
-%     'F#m19';%
-%     'Gbm19';%
-%     'Gm 20';
-%     'G#m21';%
-%     'Abm21';%
-%     'Am 22';
-%     'A#m23';%
-%     'Bbm23';%
-%     'Bm 24';
+    'D#  4';%
+    'Eb  4';%
+    'E   5';
+    'F   6';
+    'F#  7';%
+    'Gb  7';%
+    'G   8';
+    'G#  9';%
+    'Ab  9';%
+    'A  10';
+    'A# 11';%
+    'Bb 11';%
+    'B  12';
+    
+    'Cm 13';        % accordm mineur
+    'C#m14';%
+    'Dbm14';%
+    'Dm 15';
+    'D#m16';%
+    'Ebm16';%
+    'Em 17';
+    'Fm 18';
+    'F#m19';%
+    'Gbm19';%
+    'Gm 20';
+    'G#m21';%
+    'Abm21';%
+    'Am 22';
+    'A#m23';%
+    'Bbm23';%
+    'Bm 24';
     ];
 
 % on calcul la distante entre chaque chroma
