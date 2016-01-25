6e772c0cef37b463dd2b359d4c45e89d89abec70
Maxou <maxime.arbisa@gmail.com>
2016-01-25 18:57:53 +0100

    Code Maxou

---
+++ src/analyseMusic.m
@@ -0,0 +1,182 @@
+% Ce script créé les bases d'observations de référence et les compare 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear all;
+close all;
+clc;
+
+%% Fichiers de sauvegarde
+FILE_s.EXPE1_OBS    = 'chroma_obs_base.mat'; % Observations chroma
+FILE_s.EXPE1_CHORDS_PLAYS    = 'chords_base_plays.mat'; % Accords par morceaux
+FILE_s.EXPE1_CHORDS    = 'chords_base.mat'; % Accords globaux
+FILE_s.EXPE1_PROBA_CHORDS    = 'proba_chords.mat'; % Probas de passage d'un accord à l'autre
+FILE_s.EXPE1_MUSIC    = 'music.mat'; % Analyse par accords pour bibliothèque musicale 
+
+%% Parameters
+L_sec					= 0.6;	% --- Analysis window duration in seconds
+STEP_sec				= 0.3;	% --- Analysis hop size in seconds
+
+Observations = containers.Map(); % Dictionnaire contenant les vecteurs d'observations chromas pour tous les morceaux
+                                 % Keys: noms des morceaux
+                                 % Values: matrices d'observations chromas
+
+Chords = containers.Map(); % Dictionnaire contenant les accords pour chaque morceau
+                           % Keys: noms des morceaux
+                           % Values: liste contenant les accords détectés
+                           % et leur temps de début et de fin
+
+%% Extraction des vecteurs d'observation chromas 
+if 1
+    fprintf(1, 'Calcul des vecteurs d''observations chromas pour tous les morceaux: \n\n');
+    directory_function = pwd; % Garde en mémoire le répertoire des fonctions
+    
+    % Read Excel file
+    [numbers, pieces_id, everything] = xlsread('Corp_Beatles.xls', 'A:A'); % Get ID of pieces
+    [numbers, pieces_name, everything] = xlsread('Corp_Beatles.xls', 'E:E'); % Get name of pieces
+
+	% Parcours de la base de référence musicale
+    cd ./The_Beatles_test % va dans l'ensemble des albums
+    albums = dir(pwd); % récupère tous les albums
+    albums = albums(3:end); % Enlève le '.', le '..' et le '._corp_...'
+    
+    for k = 1:length(albums) % On parcourt les albums
+        cd(albums(k).name) % Va dans l'album
+        morceaux = dir(pwd); % On récupère les morceaux pour chaque album
+        morceaux = morceaux(3:end); % On enlève le '.' et le '..'
+        
+        for k = 1:length(morceaux) % On parcourt les morceaux
+            % On obtient name_file = name.wav
+            file_id = morceaux(k).name; % fichier.wav
+            
+            % On lit le fichier .mp3
+            [data_v, sr_hz] = audioread(file_id);
+            data_v = data_v(:,1); % Takes only 1st channel for stereo sounds
+
+            % Get file's detune
+            addpath(directory_function);
+            detune = f_tuning(file_id);
+            rmpath(directory_function);
+
+            % Resample
+            data_v = resample(data_v, sr_hz, floor(detune*sr_hz));
+            detune = 1;
+
+            % Calcul du tempo
+            addpath(directory_function);
+            [y_v, tempo_v] = f_rhythm(data_v(1:10000), sr_hz);
+            rmpath(directory_function);
+            tempo = mean(tempo_v)
+            figure();
+            plot(1:length(tempo_v), tempo_v);
+            
+            % Création de la base chromas et observation chromas 
+            file_id = file_id(1:end-4); % Removes '.wav' at the end
+            
+            % Map name with excel file
+            [truefalse, index] = ismember(file_id, pieces_id); % Returns the index of the id
+            file_key = lower(pieces_name{index}); % Get piece name in lower case
+            
+            str = sprintf('%s: Calcul de la matrice des observations chroma\n', file_key);
+            fprintf(1, str);
+
+            addpath(directory_function); % Ajoute le répertoire pour runner la fonction
+            L_n				= round(L_sec*sr_hz); % window duration in points
+            STEP_n			= round(STEP_sec*sr_hz); % Hop size in points
+            obs_m	= extractChroma(data_v, sr_hz, L_n, STEP_n, detune);
+            rmpath(directory_function);
+            
+            % On le met dans le dictionnaire
+            Observations(file_key) = obs_m;
+        end
+        
+        cd ../ % Sort de l'album
+    end
+    % Fin du parcours des fichiers audio
+    cd ../ % Sort du répertoire des albums
+    
+    % On sauve le dictionnaire
+    save(FILE_s.EXPE1_OBS);
+else
+    fprintf(1, 'Chargement des vecteurs d''observations chromas pour tous les morceaux... \n');
+	load(FILE_s.EXPE1_OBS);
+end
+
+
+%% Mapping avec les annotations
+if 1
+    fprintf(1, 'Mapping with annotations - Dictionnary with plays:\n\n')
+    [Accords_morceaux, Accords, Accords_mat, Proba] = mapping(Observations, STEP_sec);    
+    
+    % On sauve le dictionnaire
+    save(FILE_s.EXPE1_CHORDS_PLAYS);
+    save(FILE_s.EXPE1_CHORDS);
+    save(FILE_s.EXPE1_PROBA_CHORDS);
+else
+    fprintf(1, 'Chargement des accords et des probabilités...\n')
+	load(FILE_s.EXPE1_CHORDS_PLAYS);
+    load(FILE_s.EXPE1_CHORDS);
+    load(FILE_s.EXPE1_PROBA_CHORDS);
+end
+
+keykey = Accords_mat.keys();
+for k = 1:length(keykey)
+    Accords_mat(char(keykey(k))) = mean(Accords_mat(char(keykey(k))),2);
+end
+
+%% Analyser la bibliothèque musicale
+fprintf(1, 'Analyse de la bibliothèque musicale\n\n');
+if 1
+    directory_function = pwd; % Garde en mémoire le répertoire des fonctions
+    
+	% Parcours de la base de référence musicale
+    cd ./The_Beatles % va dans l'ensemble des albums de la bibliothèque musicale
+    albums = dir(pwd); % récupère tous les albums
+    albums = albums(3:end); % Enlève le '.' et le '..'
+    
+    for k = 1:1%length(albums) % On parcourt les albums
+        cd(albums(k).name) % Va dans l'album
+        morceaux = dir(pwd); % On récupère les morceaux pour chaque album
+        morceaux = morceaux(3:end); % On enlève le '.' et le '..'
+        
+        for k = 1:1%length(morceaux) % On parcourt les morceaux
+            % On obtient name_file = name.mp3
+            name_file = morceaux(k).name;
+            
+            % On lit le fichier .mp3
+            [data_v, sr_hz] = audioread(name_file);
+            data_v = data_v(:,1); % Takes only 1st channel for stereo sounds
+
+            % Get file's detune
+            addpath(directory_function);
+            detune = f_tuning(name_file);
+            rmpath(directory_function);
+            
+            data_v = resample(data_v, sr_hz, floor(detune*sr_hz));
+            detune = 1;
+            
+            % Création de la base chromas et observation chromas 
+            file_key = name_file(1:end-4); % Removes '.mp3' at the end
+            str = sprintf('%s: Analyse des accords\n', file_key);
+            fprintf(1, str);
+
+            addpath(directory_function); % Ajoute le répertoire pour runner la fonction
+            L_n				= round(L_sec*4*sr_hz); % window duration in points
+            STEP_n			= round(STEP_sec*4*sr_hz); % Hop size in points
+            [list_chords, list_times]	= extractChords(data_v, sr_hz, L_n, STEP_n, detune, Accords_mat, Proba);
+            rmpath(directory_function);
+            
+            % On le met dans le dictionnaire
+            Chords(file_key) = list_chords;
+        end
+        
+        cd ../ % Sort de l'album
+    end
+    % Fin du parcours des fichiers audio
+    cd ../ % Sort du répertoire des albums
+    
+    % On sauve le dictionnaire
+    save(FILE_s.EXPE1_MUSIC);
+else
+	load(FILE_s.EXPE1_MUSIC);
+end
+
+
---
+++ src/mapping.m
@@ -0,0 +1,173 @@
+function [Accords_morceaux, Accords, Accords_mat, Proba] = mapping(Observations, STEP_sec)
+% Fait le mapping entre les annotations et les accords
+% 1) pour chaque morceaux, on a le dictionnaire Accords_morceaux
+% 2) Regroupe les accords dans Accords
+% 3) Calcule les probabilité de passage d'un accord à l'autre
+
+Accords_morceaux = containers.Map(); % Dictionnaire contenant pour chaque morceau les chromas de référence pour tous les accords
+                                     % Keys: nom des morceaux
+                                     % Values: Dictionnaire avec les noms
+                                     % d'accords et les chromas associés
+
+Accords = containers.Map(); % Dictionnaire contenant les chromas de référence pour chaque accord
+                            % Keys: Accords
+                            % Values: chromas associés
+                            
+Accords_mat = containers.Map();
+
+Proba = containers.Map(); % Dictionnaire contenant les probabilités de passage d'un accord à l'autre
+                          % Keys: Accords sachant qu'il y a eu un autre
+                          % accord
+                          % Values: valeur de probabilité
+
+Proba_tot = containers.Map(); % Dictionnaire contenant le nombre total d'évènements
+                              % Keys: Accords
+                              % Values: nombre de fois qu'il a été
+                              % rencontré
+                              
+%% Trouve les annotations - lancé depuis le répertoire courant
+% Parcours de la base de référence musicale
+cd ./The_Beatles_annot % va dans l'ensemble des annotations des albums
+albums = dir(pwd); % récupère les annotations de tous les albums
+albums = albums(3:end); % Enlève le '.' et le '..'
+
+for k = 1:length(albums) % On parcourt les albums
+    cd(albums(k).name) % Va dans l'album
+    annots = dir('*.lab'); % On récupère les annotations des morceaux
+                           % pour chaque album
+    
+    for k = 1:length(annots) % On parcourt les annotations de chaque morceaux
+        name_file = annots(k).name;
+                
+        % Read annotations
+        fileID = fopen(name_file, 'r'); % read file
+        formatSpec = '%f %f %s';
+        
+        data = textscan(fileID, formatSpec);
+        fclose(fileID);
+        
+        debut = data{1,1}; % Temps de début
+        fin = data{1,2}; % Temps de fin
+        chords = data{1,3}; % Accord
+        nb_lines = length(debut); % Nb de lignes dans le document,
+        % peut aussi être length(fin) ou length(notes)
+        
+        % Retrouve la matrice d'observations chroma correspondante
+        name_file = name_file(6:end-4); % remove 01_-_ and '.lab'
+        name_file = lower(strrep(name_file, '_', ' ')); % Met au même format que les titres
+        
+        % Affiche où on est
+        str = sprintf('Mapping with annotations: %s\n', name_file);
+        fprintf(1, str);
+
+        if isKey(Observations, name_file) % s'il est déjà présent
+        % Extrait la matrice d'observations chroma correspondante
+            obs_m = Observations(name_file); % A changer suivant le nom des fichiers
+        else
+            fprintf(1, name_file);
+            break;
+        end
+        
+        %% Mapping
+        accords = containers.Map(); % Ensemble d'accords pour chaque morceau
+        former_chord = 'N';
+        
+        for k = 2:nb_lines-1 % Commence par N et finit par N
+            
+            % Trames de début et de fin
+            if floor(debut(k)/STEP_sec) == debut(k)/STEP_sec % Cas entier
+                deb_trame = debut(k)/STEP_sec; % debut en trames
+            else
+                deb_trame = floor(debut(k)/STEP_sec) + 1; % debut en trames
+            end
+            if floor(fin(k)/STEP_sec) == fin(k)/STEP_sec % Cas entier
+                fin_trame = fin(k)/STEP_sec; % fin en trames
+            else
+                fin_trame = floor(fin(k)/STEP_sec) + 1; % fin en trames
+            end
+            
+            % Récupération de l'accord
+            chord = chords{k}; % Accord - string
+            min_chord = strfind(chord, ':mi'); % renvoie l'index du mineur
+            if isempty(min_chord) % Si il n'y a pas de mineur
+                if length(chord) == 2 && (chord(2) == 'b' || chord(2) == '#') % Case b or #
+                    chord = chord(1:2);
+                else % Other case
+                    chord = chord(1);
+                end
+            else % Cas mineur, on enlève le ':'
+                chord = strcat(chord(1:min_chord-1), 'm');                                    
+            end
+            
+%             min_chord = strfind(chord, ':'); % renvoie l'index du mineur
+%             if ~isempty(min_chord) % S'il y a un ':'
+%                 chord = strcat(chord(1:min_chord-1), chord(min_chord+1:end));
+%             end
+            
+            chroma = mean(obs_m(:, deb_trame:fin_trame), 2); % On moyenne les chromas correspondant
+
+            % Matrice d'observations des accords
+            if isKey(Accords_mat, chord) % s'il est déjà présent
+                Accords_mat(chord) = [Accords_mat(chord) obs_m(:, deb_trame:fin_trame)];
+            else
+                Accords_mat(chord) = obs_m(:, deb_trame:fin_trame);
+            end
+            
+            % Déjà rencontré ? - Accords pour chaque morceau
+            if isKey(accords, chord) % s'il est déjà présent
+                accords(chord) = (accords(chord)+chroma)/2; % On moyenne les chromas obtenus
+            else
+                accords(chord) = chroma; % Nouvelle key
+            end
+            
+            % Déjà rencontré ? - Accords totaux
+            if isKey(Accords, chord) % s'il est déjà présent
+                Accords(chord) = (Accords(chord)+chroma)/2; % On moyenne les chromas obtenus
+            else
+                Accords(chord) = chroma; % Nouvelle key
+            end
+            
+            % Proba de passage entre accords
+            % Proba totale
+            if isKey(Proba_tot, chord) % s'il est déjà présent
+                Proba_tot(chord) = Proba_tot(chord) + 1; % On incrémente le nombre total
+                                                     % d'évènements pour
+                                                     % l'accord
+            else
+                Proba_tot(chord) = 1;
+            end
+            
+            % Proba de passage
+            str = sprintf('%s/%s', chord, former_chord);
+            if isKey(Proba, str) % s'il est déjà présent
+                Proba(str) = Proba(str)+1;
+            else
+                Proba(str) = 1;
+            end
+            
+            former_chord = chord;
+            
+            
+        end
+        
+        Accords_morceaux(name_file) = accords; % On met les accords pour chaque morceau
+
+    end
+    cd ../
+
+end
+cd ../
+
+% divise les probas par le nombre total d'évènements
+fprintf(1, '\n\nCalcul des probabilités de passage:\n\n');
+prob_keys = Proba.keys();
+
+% for k = 1:length(prob_keys)
+%     pattern = '/(.*)';
+%     chord = char(regexp(char(prob_keys(k)), pattern, 'match')); % cast to string and get /***
+%     chord = chord(2:end); % cast to string
+%     Proba(char(prob_keys(k))) = Proba(char(prob_keys(k)))/Proba_tot(chord);
+% end
+
+end
+
